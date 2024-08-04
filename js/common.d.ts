a1021ad324a0786a7a15956d9fcd53201c2c5af8
suXin <suxinjke@gmail.com>
2024-08-04 23:32:39 +0700

    remove previously used Rich Presence functions

--- js/common.d.ts
+++ js/common.d.ts
@@ -11,26 +11,6 @@ declare global {
   }
 }
 
-export function makeRichPresenceDisplay(value: string): string
-export function makeRichPresenceDisplay(
-  condition: string | Condition | ConditionBuilder,
-  value: string
-): string
-
 export function asciiToNumberLE(str: string): number
 
-export function makeRichPresenceLookup(params: {
-  name: string,
-  keyFormat?: 'hex' | 'dec',
-  values: Record<string | number, string>,
-  defaultAddress?: string | Condition | ConditionBuilder
-}): {
-  toString(): string
-  point(address: string | Condition | ConditionBuilder): string
-}
-
-export function makeLookup(name: string, prefix: string, obj: Record<number, string>): {
-  rich: string,
-  point(address: number): string
-}
 export function givenRangeOf(start?: number, end?: number): number[]
--- js/common.js
+++ js/common.js
@@ -1,19 +1,3 @@
-import { ConditionBuilder } from '@cruncheevos/core'
-
-export function makeLookup(name, prefix, obj) {
-  let rich = `Lookup:${name}\n`
-  for (const key in obj) {
-    rich += `0x${Number(key).toString(16).toUpperCase().padStart(2, '0')}=${obj[key]}\n`
-  }
-
-  return {
-    rich,
-    point(address) {
-      return `@${name}(0x${prefix}${address.toString(16).toUpperCase()})`
-    }
-  }
-}
-
 export function asciiToNumberLE(str) {
   if (str.length > 4) {
     throw new Error('expected length of string to be less or equal to 4')
@@ -22,42 +6,6 @@ export function asciiToNumberLE(str) {
   const val = str.split('').map(c => c.charCodeAt(0).toString(16).padStart(2, '0')).reverse().join('')
 
   return Number('0x' + val)
-}
-
-export function makeRichPresenceLookup(params) {
-  let rich = `Lookup:${params.name}`
-  for (const inputKey in params.values) {
-    const keyNumber = Number(inputKey)
-    const key =
-      Number.isNaN(keyNumber) ? inputKey :
-        params.keyFormat === 'dec' ? keyNumber :
-          keyNumber.toHexString().toUpperCase().padStart(2, '0')
-
-    rich += `\n${key}=${params.values[inputKey]}`
-  }
-
-  return {
-    name: params.name,
-    toString() {
-      return rich
-    },
-    point(input) {
-      return `@${params.name}(${input.toString()})`
-    },
-    defaultPoint() {
-      if (!params.defaultAddress) {
-        throw new Error('default address not set')
-      }
-      return params.defaultAddress
-    }
-  }
-}
-
-export function makeRichPresenceDisplay(...args) {
-  const condition = args.length >= 2 ? `?${args[0]}?` : ''
-  const value = args.length >= 2 ? args[1] : args[0]
-
-  return condition + value
 }
 
 export function givenRangeOf(start = 0, end = 0) {
