cfb433577deca4ddb8592e0a322413734572e1f2
suXin <suxinjke@gmail.com>
2026-08-31 20:26:27 +0700

    Driver 2: merge lists

--- js/Duckstation/Driver2/Driver2.js
+++ js/Duckstation/Driver2/Driver2.js
@@ -2,7 +2,129 @@
 
 import { AchievementSet, ConditionBuilder, RichPresence, define as $, pauseIf, trigger, andNext, orNext, resetNextIf, resetIf, once, measuredIf } from '@cruncheevos/core'
 
-import * as lists from './lists.js'
+const pursuits = /** @type {const} */ ([
+  [0x46, 10, 'Chicago'],
+  [0x4e, 5, 'Havana'],
+  [0x56, 10, 'Las Vegas'],
+  [0x5e, 10, 'Rio'],
+])
+
+const getaways = /** @type {const} */ ([
+  [0x66, 'Chicago - Downtown'],
+  [0x67, 'Chicago - Wrigleyville'],
+  [0x6e, 'Havana - Necropolis de Colon'],
+  [0x6f, 'Havana - Capitolio'],
+  [0x76, 'Las Vegas - Downtown'],
+  [0x77, 'Las Vegas - Upper Strip'],
+  [0x7e, 'Rio - Centro'],
+  [0x7f, 'Rio - Copacabana', 'damageOk'],
+])
+
+const gateRaces = /** @type {const} */ ([
+  [0x86, 'Chicago - Greektown'],
+  [0x87, 'Chicago - Grant Park'],
+  [0x8e, 'Havana - Necropolis de Colon'],
+  [0x8f, 'Havana - Old Havana'],
+  [0x96, 'Las Vegas - Lakeside'],
+  [0x97, 'Las Vegas - Mid Strip'],
+  [0x9e, 'Rio - Copacabana'],
+  [0x9f, 'Rio - Santa Tereza'],
+])
+
+const trailblzers = /** @type {const} */ ([
+  [0x104, 'Chicago - Grant Park'],
+  [0x105, 'Chicago - Downtown'],
+  [0x10c, 'Havana - Old Havana'],
+  [0x10d, 'Havana - Vedado'],
+  [0x114, 'Las Vegas - Downtown'],
+  [0x115, 'Las Vegas - Upper Strip'],
+  [0x11c, 'Rio - Leblon'],
+  [0x11d, 'Rio - Praca da Bandeira'],
+])
+
+const checkpointRaces = /** @type {const} */ ([
+  [0xc4, '01:29.00', 10, 'Chicago - Downtown'],
+  [0xc5, '00:59.50', 5, 'Chicago - Meigs Field'],
+  [0xcc, '00:58.20', 10, 'Havana - The Docks'],
+  [0xcd, '01:57.00', 5, 'Havana - Old Havana'],
+  [0xd4, '01:17.77', 5, 'Las Vegas - North Vegas'],
+  [0xd5, '01:09.99', 5, 'Las Vegas - Lakeside'],
+  [0xdc, '01:46.50', 5, 'Rio - Lagoa Rodrigo de Freitas'],
+  [0xdd, '01:28.00', 5, 'Rio - Praca da Bandeira'],
+])
+
+const survivalRaces = /** @type {const} */ ([
+  [0x124, 25, 'Chicago'],
+  [0x12c, 30, 'Havana'],
+  [0x134, 30, 'Las Vegas', 'no-leaderboard'],
+  [0x13c, 30, 'Rio', 'no-leaderboard'],
+])
+
+const secretRaces = /** @type {const} */ ([
+  [[0x1e0, 0x1e1], '01:30.00', 10, 'Secret Mountain Pass', 'lapTime'],
+  [[0x1e2, 0x1e3], '02:15.00', 5, 'Secret Race Track'],
+])
+
+const secretCars = /** @type {const} */ ([
+  [
+    [0x32, 0x33],
+    [0x1194, 0x11301000, 0x31],
+    'Chicago Secret Car',
+    'Chicago',
+    'Take a Drive and get inside secret car in Chicago Stadium',
+    {
+      title: 'Cannonball Chicago',
+      description: 'After getting inside the secret car in Chicago Stadium, deliver it to the Airport, near the aircraft. No tail',
+      coords: {
+        top: 0xFFF9ED1B,
+        bottom: 0xFFF97300,
+        left: 0x1E4A6,
+        right: 0x1F7BF
+      }
+    }
+  ],
+  [
+    [0x34, 0x35],
+    [0x1000, 0xaf01000, 0x21],
+    'Havana Secret Car',
+    'Havana',
+    'Take a Drive and get inside secret car in Havana Underground Parking',
+    {
+      title: 'Mini Delivery',
+      description: 'After getting inside the secret car in Havana underground parking, deliver it to South-Eastern Docks. No tail',
+      coords: {
+        top: 0xFFFAF4F6,
+        bottom: 0xFFFA7200,
+        left: 0x51C00,
+        right: 0x55E00
+      }
+    }
+  ],
+  [
+    [0x36, 0x37],
+    [0x1000, 0x10001000, 0xFF82002B, 0x0D],
+    'Las Vegas Secret Truck',
+    'LasVegas',
+    'Take a Drive and get inside secret truck in Las Vegas',
+    {
+      title: 'Cannonball Las Vegas',
+      description: 'After getting inside the secret truck in Las Vegas, deliver it to Ruins north-west of Ghost Town, near the Train Tunnel. No tail',
+      coords: {
+        top: 0xD9500,
+        bottom: 0xD7B00,
+        left: 0xFFFF1000,
+        right: 0xFFFF2300
+      }
+    }
+  ],
+  [
+    [0x38, 0x39],
+    [0x0B54, 0x0ED81000, 0x47],
+    'Rio Secret Truck',
+    'Rio',
+    'Take a Drive and get inside the secret truck in Rio. What are you gonna do with it?'
+  ],
+])
 
 const gameType = {
   mission: 0,
@@ -83,12 +205,12 @@ const missionTitles = {
 
   // hacky dumb normalization
   ...[
-    ...lists.pursuits,
-    ...lists.getaways,
-    ...lists.gateRaces,
-    ...lists.trailblzers,
-    ...lists.checkpointRaces.map(x => [x[0], x[2]]),
-    ...lists.survivalRaces.map(x => [x[0], x[2]]),
+    ...pursuits,
+    ...getaways,
+    ...gateRaces,
+    ...trailblzers,
+    ...checkpointRaces.map(x => [x[0], x[2]]),
+    ...survivalRaces.map(x => [x[0], x[2]]),
   ].reduce((prev, array) => {
     prev[array[0]] = array[1]
     return prev
@@ -828,7 +950,7 @@ missionAchievement(0x27, 10, {
 })
 missionAchievement(0x28, 10, { title: 'In Which Lenny Finally Gets Caught', type: 'win_condition' })
 
-lists.pursuits.forEach((x, i) => {
+pursuits.forEach((x, i) => {
   const [missionId, points, title] = x
 
   set.addAchievement({
@@ -850,7 +972,7 @@ lists.pursuits.forEach((x, i) => {
   })
 })
 
-lists.getaways.forEach((x, i) => {
+getaways.forEach((x, i) => {
   const [missionId, title] = x
   const noDamage = Boolean(x[2]) === false
 
@@ -883,7 +1005,7 @@ lists.getaways.forEach((x, i) => {
   })
 })
 
-lists.gateRaces.forEach((x, i) => {
+gateRaces.forEach((x, i) => {
   const [missionId, title] = x
 
   const conditions = deltaFix => multiRegionalConditions(region => {
@@ -934,7 +1056,7 @@ set.addAchievement({
       c.protections(),
 
       c.hasCompletedMissionInGame({
-        missionId: lists.gateRaces.map(([missionId]) => missionId),
+        missionId: gateRaces.map(([missionId]) => missionId),
         scoreTarget: 100,
         gameTypeId: gameType.gateRace
       }),
@@ -942,7 +1064,7 @@ set.addAchievement({
   })
 })
 
-lists.trailblzers.forEach((x, i) => {
+trailblzers.forEach((x, i) => {
   const [missionId, title] = x
 
   const conditions = deltaFix => multiRegionalConditions(region => {
@@ -993,7 +1115,7 @@ set.addAchievement({
       c.protections(),
 
       c.hasCompletedMissionInGame({
-        missionId: lists.trailblzers.map(([missionId]) => missionId),
+        missionId: trailblzers.map(([missionId]) => missionId),
         scoreTarget: 100,
         gameTypeId: gameType.trailblazer
       }),
@@ -1001,7 +1123,7 @@ set.addAchievement({
   })
 })
 
-lists.checkpointRaces.forEach((x, i) => {
+checkpointRaces.forEach((x, i) => {
   const [missionId, timeTarget, points, title] = x
 
 
@@ -1051,7 +1173,7 @@ lists.checkpointRaces.forEach((x, i) => {
   })
 })
 
-lists.survivalRaces.forEach((x, i) => {
+survivalRaces.forEach((x, i) => {
   const [missionId, secondsTarget, title, noLeaderboard] = x
 
   set.addAchievement({
@@ -1099,7 +1221,7 @@ lists.survivalRaces.forEach((x, i) => {
   }
 })
 
-lists.secretRaces.forEach((x, i) => {
+secretRaces.forEach((x, i) => {
   const [missionId, targetTime, points, title, checkLapTime] = x
 
   const checkingLapTime = Boolean(checkLapTime)
@@ -1205,7 +1327,7 @@ lists.secretRaces.forEach((x, i) => {
   })
 })
 
-for (const x of lists.secretCars) {
+for (const x of secretCars) {
   const [missionId, statData, title, cityName, description, deliveryChallenge] = x
 
   set.addAchievement({
